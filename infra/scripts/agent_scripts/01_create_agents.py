3666595d190b07627749e77a5cb8f48bd3de8c8b
nchandhi <nalini.chandhi@microsoft.com>
2025-08-15 17:32:18 -0700

    test python putpit

--- infra/scripts/agent_scripts/01_create_agents.py
+++ infra/scripts/agent_scripts/01_create_agents.py
@@ -1,29 +1,29 @@
-# import os
-from azure.ai.projects import AIProjectClient
-from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
+# # import os
+# from azure.ai.projects import AIProjectClient
+# from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
 
-KEY_VAULT_NAME = 'kv_to-be-replaced' # This will be replaced in the script
-MANAGED_IDENTITY_CLIENT_ID = 'mici_to-be-replaced'
-ai_project_endpoint = 'project_endpoint_to-be-replaced'  # This will be replaced in the script
+# KEY_VAULT_NAME = 'kv_to-be-replaced' # This will be replaced in the script
+# MANAGED_IDENTITY_CLIENT_ID = 'mici_to-be-replaced'
+# ai_project_endpoint = 'project_endpoint_to-be-replaced'  # This will be replaced in the script
 
-# Initialize the AI project client
+# # Initialize the AI project client
+# # project_client = AIProjectClient(
+# #     endpoint= ai_project_endpoint,
+# #     credential=DefaultAzureCredential(),
+# # )
+
 # project_client = AIProjectClient(
 #     endpoint= ai_project_endpoint,
-#     credential=DefaultAzureCredential(),
+#     credential=ManagedIdentityCredential(client_id=MANAGED_IDENTITY_CLIENT_ID),
 # )
 
-project_client = AIProjectClient(
-    endpoint= ai_project_endpoint,
-    credential=ManagedIdentityCredential(client_id=MANAGED_IDENTITY_CLIENT_ID),
-)
-
-instructions='''You are a helpful assistant'''
-with project_client:
-    agents_client = project_client.agents
-    agent = agents_client.create_agent(
-        model='gpt-4o-mini',
-        name="my-agent2",
-        instructions=instructions
-    )
-    print(agent.id)
+# instructions='''You are a helpful assistant'''
+# with project_client:
+#     agents_client = project_client.agents
+#     agent = agents_client.create_agent(
+#         model='gpt-4o-mini',
+#         name="my-agent2",
+#         instructions=instructions
+#     )
+#     print(agent.id)
 print('12345678')    
